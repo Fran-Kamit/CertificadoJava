b2a917e44a5622b35803399d8dfe21e4e491ef7f
Francisco José <65421353+Fran-Kamit@users.noreply.github.com>
2023-04-11 20:03:50 +0200

    Add files via upload

---
+++ T09/ActividadPropuesta9_01/src/App.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Integer n;
+        Scanner sc = new Scanner(System.in);
+
+        Lista x = new Lista();
+        
+        System.out.print("Introducir número (negativo para salir): ");
+        n = sc.nextInt();
+
+        while (n >= 0) {
+            x.apilar(n);
+            System.out.print(" Introduzca número: ");
+            n = sc.nextInt();
+        }
+        n = x.desapilar();
+
+        while (n != null){
+            System.out.print(n + " ");
+            n = x.desapilar();
+        }
+        System.out.println("");
+
+        sc.close();
+    }
+}
