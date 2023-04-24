6495c16eca3ad5c392c8eb791c977769cd9681bb
Francisco José <65421353+Fran-Kamit@users.noreply.github.com>
2023-04-24 20:43:53 +0200

    Add files via upload

---
+++ T11 - Spring/prueba1_spring - archivo de propiedades externo/src/main/java/com/spring/prueba1_spring/Empleados.java
@@ -0,0 +1,8 @@
+package com.spring.prueba1_spring;
+
+public interface Empleados {
+    public String getResponsabilidades();
+    public String getDocumento();
+    public String getEmailDepartamental();
+    public String getEmpresa();
+}
