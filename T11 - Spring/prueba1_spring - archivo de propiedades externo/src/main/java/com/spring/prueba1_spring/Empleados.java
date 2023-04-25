75a7e8ba82b2a58040a74e6a67e11cd5c0d59d8c
Francisco José <65421353+Fran-Kamit@users.noreply.github.com>
2023-04-25 16:40:51 +0200

    Delete T11 - Spring/prueba1_spring - archivo de propiedades externo directory

--- T11 - Spring/prueba1_spring - archivo de propiedades externo/src/main/java/com/spring/prueba1_spring/Empleados.java
+++
@@ -1,8 +0,0 @@
-package com.spring.prueba1_spring;
-
-public interface Empleados {
-    public String getResponsabilidades();
-    public String getDocumento();
-    public String getEmailDepartamental();
-    public String getEmpresa();
-}
