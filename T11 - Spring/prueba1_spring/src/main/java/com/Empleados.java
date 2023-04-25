ae80126246969f670888399cc28dc08054b91570
Francisco José <65421353+Fran-Kamit@users.noreply.github.com>
2023-04-25 16:40:59 +0200

    Delete T11 - Spring/prueba1_spring directory

--- T11 - Spring/prueba1_spring/src/main/java/com/Empleados.java
+++
@@ -1,8 +0,0 @@
-package com;
-
-public interface Empleados {
-    public String getResponsabilidades();
-    public String getDocumento();
-    public String getEmailDepartamental();
-    public String getEmpresa();
-}
